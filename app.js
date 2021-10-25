6f5dc858f67f123c82810074300074803a1f577a
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-25 20:20:57 +0200

    add app.get all articles

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 //requiring modules installed
 const express = require('express');
+const ejs = require('ejs')
 const mongoose = require('mongoose');  
-//middleware
-const app = express();
-app.set('view engine', 'ejs');
-app.use(express.urlencoded()); //Parse URL-encoded bodies
 
-//TODO
+const app = express();
+
+//middleware
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true})); //Parse URL-encoded bodies
+app.use(express.static("public"));
+
+
+//Mongoose connection
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true});
+
+const articleSchema = {
+  title: String,
+  content: String,
+  resource: String
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
+//Routes
+app.get('/articles', function(req,res){
+  Article.find({}, function(err, AllArticles){
+    if(!err){
+      res.send(AllArticles)
+    }else{
+      console.log(err)
+    }
+  });
+});
 
 //server 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+app.listen(4000, () => console.log('app listening on port 4000!'));
