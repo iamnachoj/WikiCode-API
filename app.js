e79c6ae2d3bebe85431e36b6a482805fe94baf7e
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-25 18:17:13 +0200

    add some comments

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 //requiring modules installed
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');  
+const mongoose = require('mongoose');  
 //middleware
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 
-//
+//TODO
 
 //server 
 app.listen(4000, () => console.log('Example app listening on port 4000!'));
